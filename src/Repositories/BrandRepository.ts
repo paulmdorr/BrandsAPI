d2a2a9d830bf10216844dba4a50f157f2649be70
Paul M Dorr <paul.mdorr@gmail.com>
2019-01-17 01:55:36 -0300

    Add findByCategory and create methods

--- src/Repositories/BrandRepository.ts
+++ src/Repositories/BrandRepository.ts
@@ -1,15 +1,35 @@
 import { Pool, QueryResult } from 'pg'
-import { IRepository } from './IRepository'
+import { IRepositoryWrite } from './IRepositoryWrite'
 import Brand from '../Models/Brand'
-import BaseRepository from './BaseRepository';
+import BaseRepository from './BaseRepository'
 
-class BrandRepository extends BaseRepository<Brand> {
+class BrandRepository extends BaseRepository<Brand> implements IRepositoryWrite<Brand> {
   protected table: string = 'brands'
 
   public async findByCategoryId(categoryId: string): Promise<Brand[]> {
-    const res = await this.datasource.query(`SELECT * FROM ${this.table}`)
+    const res = await this.datasource.query(
+      `SELECT * FROM ${this.table} WHERE category_id = '${categoryId}'`,
+    )
 
     return Promise.resolve(res.rows.map((row) => new Brand(row)))
+  }
+
+  public async create(item: Brand): Promise<Brand | boolean> {
+    const query = `INSERT INTO ${this.table}(name, category_id)
+      VALUES('${item.name}', '${item.categoryId}') RETURNING id`
+    const res = await this.datasource.query(query)
+
+    item.id = res.rows[0].id
+
+    return Promise.resolve(new Brand(item))
+  }
+
+  public async update(id: string, item: Brand): Promise<Brand | boolean> {
+    return this.datasource.query('test')
+  }
+
+  public async delete(id: string): Promise<boolean> {
+    return this.datasource.query('test')
   }
 }
 
